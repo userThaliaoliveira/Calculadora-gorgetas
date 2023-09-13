c531397dc3b4a95fb319fb50d27ee803dbf273f2
userThaliaoliveira <thaliaferreira@id.uff.br>
2023-09-13 18:18:42 -0300

    updates

--- README.md
+++ README.md
@@ -21,21 +21,27 @@ Projeto refatorado a partir do codigo fornecido pela Thaliaoliveira
 Se você deseja contribuir com este projeto, siga os passos abaixo:
 
 - Faça um fork do repositório.
-bash
+```bash
   Clique no botão "Fork" no canto superior direito da página do repositório para criar uma cópia do projeto na sua conta do GitHub.
-
+```
 - Crie um novo branch com a sua funcionalidade:
 
-bash
+```bash
 git checkout -b minha-funcionalidade
 Faça commit das suas alterações:
+```
 
-
-bash
+```bash
 git commit -m 'Adicionando uma nova funcionalidade'
 Faça push para o branch:
+```
 
-
-bash
+```bash
 git push origin minha-funcionalidade
-Abra um pull request no GitHub.
+Abra um pull request no GitHub.
+```
+
+- Contato :email:
+Se tiver alguma dúvida ou sugestão, sinta-se à vontade para entrar em contato.
+
+- Github Profile: [Júlio César Silvestre](https://github.com/DayBellatto) :octocat:
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+//Classe que representa a calculadora de gorjeta
+class TipCalculator {
+    private bill: number;
+    private serviceQuality: number;
+    private numOfPeople: number;
+
+    constructor(bill: number, serviceQuality: number, numOfPeople: number) {
+        this.bill = bill;
+        this.serviceQuality = serviceQuality;
+        this.numOfPeople = numOfPeople;
+        //console.log(TipCalculator)
+    }
+
+    // Método para calcular a gorjeta
+    calculateTip(): number {
+        if (this.bill <= 0 || this.serviceQuality <= 0 || this.numOfPeople <= 0) {
+            return 0; // Evitar divisões por zero
+        }
+
+        const tip = (this.bill * this.serviceQuality) / this.numOfPeople;
+        console.log("teste", tip)
+
+        return tip;
+    }
+
+}
+
+//Função para lidar com o envio do formulário
+function handleFormSubmit(event: Event): void {
+   event.preventDefault();
+
+   const billInput: HTMLInputElement | null = document.getElementById('bill') as HTMLInputElement;
+   const serviceQualityInput: HTMLInputElement | null = document.getElementById('serviceQual') as HTMLInputElement;
+   const numOfPeopleInput: HTMLInputElement | null = document.getElementById('people') as HTMLInputElement;
+   const tipDisplay: HTMLElement | null = document.getElementById('tip');
+
+   if (!billInput || !serviceQualityInput || !numOfPeopleInput || !tipDisplay) {
+       console.error('Element not found.');
+       return;
+   }
+
+    const bill: number = parseFloat(billInput.value);
+    const serviceQuality: number = parseFloat(serviceQualityInput.value);
+    const numOfPeople: number = parseFloat(numOfPeopleInput.value);
+
+   const calculator = new TipCalculator(bill, serviceQuality, numOfPeople);
+   const tipAmount = calculator.calculateTip();
+
+   tipDisplay.innerHTML = `: $${tipAmount.toFixed(2)}`;
+}
+
+//Adicione um ouvinte de evento para o envio do formulário
+const tipsForm: HTMLFormElement | null = document.getElementById('tipsForm') as HTMLFormElement;
+if (tipsForm) {
+   tipsForm.addEventListener('submit', handleFormSubmit);
+}
+
+
+//let x = 10;
+//console.log(`:$${TipAmount.fixed(2)}`);
